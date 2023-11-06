d9eaa60dcf478050f5d72b853c587fb4b69c2200
zyang91 <zyang91@binghamton.edu>
2023-11-06 18:08:39 -0500

    updated

--- main.py
+++ main.py
@@ -6,13 +6,24 @@ import customtkinter as ctk
 def start_download():
     try:
         ytlink = link.get()
-        ytObject = YouTube(ytlink)
+        ytObject = YouTube(ytlink, on_progress_callback=on_progress)
         video= ytObject.streams.get_highest_resolution()
+        title.configure(text=ytObject.title, text_color="green")
         video.download()
         finish.configure(text="Finished Downloading")
     except:
         finish.configure(text="Error", text_color="red")
     
+
+def on_progress(stream, chunk, bytes_remaining):
+    total_size = stream.filesize
+    bytes_downloaded = total_size - bytes_remaining
+    percentage_of_completion = bytes_downloaded / total_size * 100
+    per=str(int(percentage_of_completion)) 
+    progress.configure(text=per+"%")
+    progress.update()
+
+    progressBar.set(float(percentage_of_completion)/100)
 
 #GUI setup
 ctk.set_appearance_mode("System")
@@ -35,6 +46,16 @@ link.pack()
 # Finished Downloading
 finish = ctk.CTkLabel(app, text="")
 finish.pack()
+
+# progress percentage
+progress = ctk.CTkLabel(app, text="0%")
+progress.pack()
+
+progressBar = ctk.CTkProgressBar(app, width=400)
+progressBar.set(0)
+progressBar.pack(padx=10, pady=10)
+
+
 
 # Download button
 download = ctk.CTkButton(app, text="Download", command= start_download)
