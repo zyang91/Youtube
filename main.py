85e086c069747a6516b37253e99fa4a706819b00
zyang91 <zyang91@binghamton.edu>
2023-11-06 13:37:25 -0500

    Set up GUI

---
+++ main.py
@@ -0,0 +1,13 @@
+from pytube import YouTube
+import tkinter
+import customtkinter as ctk
+
+#GUI setup
+ctk.set_appearance_mode("System")
+ctk.set_default_color_theme("blue")
+
+#GUI frame
+app= ctk.CTk()
+app.title("Youtube Downloader")
+app.geometry("720x480")
+
